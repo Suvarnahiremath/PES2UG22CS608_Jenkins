3547e2fe880b3fc94d06655b87bdc3902d059db7
Suvarnahiremath <hiremathvishwaradhya005@gmail.com>
2025-03-11 23:31:27 +0530

    changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout<< "Hey this is Suvarna\n ";
-  COUT << "My friend is Vijju\n";
+  cout << "My friend is Vijju\n";
   return 0;
 }
